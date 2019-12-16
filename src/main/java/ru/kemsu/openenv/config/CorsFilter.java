c933167bc61f83b422a0793bed0dc2fe3ab6c5d5
Nikolay Vorobev <Worik42@yandex.ru>
2019-12-16 22:34:00 +0700

    fix cors origin

--- src/main/java/ru/kemsu/openenv/config/CorsFilter.java
+++ src/main/java/ru/kemsu/openenv/config/CorsFilter.java
@@ -1,48 +1,59 @@
 package ru.kemsu.openenv.config;
 
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 
-@Component
-public class CorsFilter implements Filter {
+@Configuration
+@EnableWebMvc
+public class CorsFilter implements Filter, WebMvcConfigurer {
+
 
     @Override
-    public void init(final FilterConfig filterConfig) throws ServletException {
-
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 
     @Override
-    public void doFilter(final ServletRequest req, final ServletResponse res,
-                         final FilterChain chain) throws IOException, ServletException {
-        final HttpServletResponse response = (HttpServletResponse) res;
-        final HttpServletRequest request = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-        response.setHeader("Access-Control-Allow-Methods",
-                "POST, GET, PUT, OPTIONS, DELETE, PATCH");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+        System.out.println("WebConfig; " + request.getRequestURI());
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, x-auth-token, X-Requested-With,observe");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-auth-token, Content-Type, Accept");
-        response.setHeader("Access-Control-Expose-Headers", "x-auth-token, Content-Type");
-
-        if (request.getMethod().equals("OPTIONS")) {
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Expose-Headers", "Authorization");
+        response.addHeader("Access-Control-Expose-Headers", "responseType");
+        response.addHeader("Access-Control-Expose-Headers", "observe");
+        System.out.println("Request Method: " + request.getMethod());
+        if (!(request.getMethod().equalsIgnoreCase("OPTIONS"))) {
             try {
-                response.getWriter().print("OK");
-                response.getWriter().flush();
-            } catch (IOException e) {
+                chain.doFilter(req, res);
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         } else {
-            chain.doFilter(request, response);
+            System.out.println("Pre-flight");
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "Access-Control-Expose-Headers" + "Authorization, content-type," +
+                    "USERID" + "ROLE" +
+                    "access-control-request-headers,access-control-request-method,accept,origin,authorization,x-requested-with,responseType,observe");
+            response.setStatus(HttpServletResponse.SC_OK);
         }
-    }
-
-    @Override
-    public void destroy() {
 
     }
+
 }
